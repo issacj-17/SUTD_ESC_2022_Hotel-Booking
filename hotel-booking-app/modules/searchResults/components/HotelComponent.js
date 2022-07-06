7dfe52d91d6454b4a8b495c8d0af2f73a889f529
Emmanuel J Lopez <emmanuelj9530@gmail.com>
2022-07-06 22:40:21 +0800

    Squashed commit of the following:
    commit 0905732b54b998df917e2be7ab185be1622c5578
    Author: Emmanuel J Lopez <emmanuelj9530@gmail.com>
    Date:   Wed Jul 6 22:37:19 2022 +0800
    
        base routing complete
    
    commit 79ef777d53d939b7a38beece6310827e835dfcd7
    Author: haydenang <64570131+haydenang@users.noreply.github.com>
    Date:   Wed Jul 6 22:33:33 2022 +0800
    
        Cleaned up the code
    
    commit f4cfed8153bfdbc4a0353f0a28ab66ff04b2e1d7
    Author: Emmanuel J Lopez <emmanuelj9530@gmail.com>
    Date:   Wed Jul 6 22:13:25 2022 +0800
    
        routing

--- hotel-booking-app/modules/searchResults/components/HotelComponent.js
+++ hotel-booking-app/modules/searchResults/components/HotelComponent.js
@@ -9,7 +9,7 @@ returns the HTML elements, displaying the information of each hotel, link to all
 @returns - HTML to be displayed
 */
 
-function hotelElem({ hotel }) {
+function hotelElem({ hotel,searchDetails }) {
     return (
       <>
         <div className={styles.hotelElemDiv}>
@@ -18,7 +18,15 @@ function hotelElem({ hotel }) {
           {/* link to hotel details page with relevant hotelId */}
           <Link href={{
             pathname: "/hotelDetails",
-            query: {hotelId: hotel.id}
+            query: {hotelId: hotel.id,
+              destination:searchDetails.destination,
+              checkInDate: searchDetails.checkInDate,
+              checkOutDate: searchDetails.checkOutDate,
+              rooms: searchDetails.rooms,
+              adults: searchDetails.adults,
+              children: searchDetails.children
+                  
+            }
           }}>
             <a className={styles.selectButton}>Select</a>
           </Link>
