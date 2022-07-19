8fdf9ca588be6d49107090f061c58e9ef4ceb536
haydenang <64570131+haydenang@users.noreply.github.com>
2022-07-20 03:44:04 +0800

    Done up UI for displaying of Hotel Pictures
    -Implemented a Carousell Component from Bootstrap
    -Customized it to fit 5 pictures of the hotel

--- hotel-booking-app/modules/hotelDetails/components/displayHotelDetails.js
+++ hotel-booking-app/modules/hotelDetails/components/displayHotelDetails.js
@@ -1,6 +1,7 @@
 import styles from '../../../styles/displayHotelDetails.module.css'
 import Link from 'next/link' //this is how to route in next.js instead of <a></a>
 import Router,{useRouter} from 'next/router' //Used to pass Props between pages
+import Slider from './imageCarousel'
 //Pass in a props object (essentially the hotel_detail object)
 
 const hotelDetailsComp = (props) => {
@@ -12,6 +13,24 @@ const hotelDetailsComp = (props) => {
     const price = props.roomData.rooms[0].lowest_price;
     const img_url = props.roomData.rooms[0].images[0].url;
     const searchDetails = props.searchDetails;
+
+    //Unpack Image url
+    const hotelImageJson = props.selectedHotelData.image_details
+    const numOfHotelImages = props.selectedHotelData.image_details.count;
+    let listOfHotelImagesUrl = [];
+    console.log(numOfHotelImages-numOfHotelImages%5)
+    
+    for (let i=0; i<numOfHotelImages-(numOfHotelImages%5); i+=5){
+        let setOfHotelImagesUrl = [];
+        let prefix = hotelImageJson.prefix
+        let suffix = hotelImageJson.suffix
+        
+        for (let j=i;j<i+5;j++){
+            setOfHotelImagesUrl.push(prefix+j+suffix)
+        }
+        listOfHotelImagesUrl.push(setOfHotelImagesUrl)
+    }
+    console.log(listOfHotelImagesUrl)
     
     
     
@@ -43,23 +62,27 @@ const hotelDetailsComp = (props) => {
     return (
         <div>
             <h1 className={styles.hotelName}>Name of Hotel : {hotelName} , this is from API call</h1>
+            
+            <Slider listOfImagesUrl={ listOfHotelImagesUrl }></Slider>
+            
             <div className={styles.body}>
                 <div className={styles.card}>
-                    <h1>Description</h1>
-                    {description}
-                </div>
-                <div className={styles.card}>
-                    <h1>{roomType}</h1>
-                    
-                    <img className={styles.roomImage} src={img_url}></img>
-                    <p>Price of {roomType} is : {price}</p>
-                    <a onClick={() => sendProps()}>Select</a>
-                    
-                </div>
-                <div className={styles.card}>
                     <h1>Location</h1>
-                    <p>{location}</p>
+                    <p>{location}</p>   
                 </div>
+                <div className={styles.card +" card"} id="description">
+                    <h1 >Description</h1>
+                    <div dangerouslySetInnerHTML={{__html: description}}/>
+                </div>
+
+            </div>
+            <div className={styles.card}>
+                <h1>{roomType}</h1>
+                
+                <img className={styles.roomImage} src={img_url}></img>
+                <p>Price of {roomType} is : {price}</p>
+                <a onClick={() => sendProps()}>Select</a>
+                    
             </div>
         </div>
     );
